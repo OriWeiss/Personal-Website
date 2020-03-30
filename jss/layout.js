31eec13f215ff24fedcd5c20dfbd0fdf9ed6793d
OriWeiss <oriweiss212@gmail.com>
2020-03-30 00:34:15 -0700

    Added flexbox to bio page

--- jss/layout.js
+++ jss/layout.js
@@ -1,5 +1,12 @@
 $(function () {
+    /*INDEX------------------------------------------------------------------------*/
     $('.travel').css({ "padding-top": $(window).height() / 6, "padding-bottom": $(window).height() / 6 })
     $('.motorsport').css({ "padding-top": $(window).height() / 6, "padding-bottom": $(window).height() / 6 })
     $('.projects').css({ "padding-top": $(window).height() / 6, "padding-bottom": $(window).height() / 6 })
+    /*------------------------------------------------------------------------*/
+    
+    /*BIO------------------------------------------------------------------------*/
+    $('.facts').css({ "padding-top": $(window).height() / 6, "padding-bottom": $(window).height() / 6 })
+
+    /*------------------------------------------------------------------------*/
 });
