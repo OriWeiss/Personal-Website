a001583a4aacbbd05f4e92329e60a76fa66ddbf4
Ori Weiss <oriweiss212@gmail.com>
2020-05-28 14:33:35 -0700

    Modifications to index animations

--- jss/index-animations.js
+++ jss/index-animations.js
@@ -19,7 +19,7 @@ $(function () {
     var haveDisplayed = false;
     
    $(window).scroll(function () {
-        if (!haveDisplayed && isInViewport(document.querySelector('#stairs-img')) ) {
+        if (!haveDisplayed && screen.width < 750 && isInViewport(document.querySelector('#stairs-img')) ) {
             haveDisplayed = true;
         // Wrap every letter in a span
         
@@ -60,6 +60,51 @@ $(function () {
 
             
         }
+
+        else if (!haveDisplayed && screen.width > 750 && isInViewport(document.querySelector('#facts-desc')) ) {
+            haveDisplayed = true;
+        // Wrap every letter in a span
+        
+            
+                $(".facts h2").css({ display: "block" });
+                
+                    // Wrap every letter in a span
+                var textWrapper = document.querySelector('.ml14 .letters');
+                textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+                anime.timeline({
+                    loop: 1,
+                    /*opacity: 0*/
+                })
+                .add({
+                  targets: '.ml14 .line',
+                  scaleX: [0,1],
+                  opacity: [0.5,1],
+                  easing: "easeInOutExpo",
+                  duration: 2000
+                }).add({
+                  targets: '.ml14 .letter',
+                  opacity: [0,1],
+                  translateX: [40,0],
+                  translateZ: 0,
+                  scaleX: [0.3, 1],
+                  easing: "easeOutExpo",
+                  duration: 800,
+                  offset: '-=600',
+                  delay: (el, i) => 150 + 25 * i
+                }).add({
+                  targets: '.ml14',
+                  opacity: 1,
+                  duration: 1000,
+                  easing: "easeOutExpo",
+                  delay: 1000
+                });
+
+            
+        }
+
+
+
     });
 
     anime.timeline({loop: 2})
